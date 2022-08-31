6d78fe4ce2253028420c85c236496b3b76b9078f
venonh2 <brendow-evan@hotmail.com>
2022-08-30 22:16:07 -0300

    feat: error boundary

--- App.tsx
+++ App.tsx
@@ -1,16 +1,17 @@
 import "react-native-gesture-handler";
-import {
-  TapGestureHandler,
-  RotationGestureHandler,
-} from "react-native-gesture-handler";
+import ErrorBoundary from "react-native-error-boundary";
 import { StatusBar } from "expo-status-bar";
 import { Routes } from "./src/routes";
 
+const errorHandler = (error: Error, stackTrace: string) => {
+  console.log(error, stackTrace);
+};
+
 export default function App() {
   return (
-    <>
+    <ErrorBoundary onError={errorHandler}>
       <StatusBar style="auto" />
       <Routes />
-    </>
+    </ErrorBoundary>
   );
 }
